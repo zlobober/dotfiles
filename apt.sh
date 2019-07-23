7a0b8de0c4501d6bfe279902e50a9bd3a934f071
Maxim Akhmedov <max42@yandex-team.ru>
2019-07-23 16:18:19 +0300

    Improvements.

--- apt.sh
+++ apt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get install g++-8 clang++-6 gdb vim git subversion tmux
-apt-get install chromium-browser
+apt-get install g++-8 clang++-8 gdb vim git subversion tmux cmake
+# apt-get install chromium-browser
--- go.sh
+++ go.sh
@@ -4,6 +4,7 @@
 
 # Setup vim
 cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim "+VundleInstall"
